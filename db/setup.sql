b45aaee5c37bf9750e475cd04d811be169e25772
Christopher Thornton <christopher@SWC-Mac-Mini.local>
2012-05-30 13:40:32 -0700

    modified the setup SQL file

--- db/setup.sql
+++ db/setup.sql
@@ -1,18 +1,5 @@
-DROP TABLE IF EXISTS `cards`;
-CREATE TABLE `cards` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `person_id` int(11) NOT NULL,
-  `status` int(11) NOT NULL DEFAULT '1',
-  `number` char(15) NOT NULL,
-  `expiration` date NOT NULL,
-  `balance` decimal(14,2) NOT NULL,
-  `created_at` datetime,
-  `updated_at` datetime,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
 DROP TABLE IF EXISTS `persons`;
+
 CREATE TABLE `persons` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `company_id` int(11) DEFAULT NULL,
@@ -25,11 +12,55 @@ CREATE TABLE `persons` (
   `zipcode` char(5) DEFAULT NULL,
   `ssn` char(10) DEFAULT NULL,
   `phone` char(10) DEFAULT NULL,
-  `created_at` datetime,
-  `updated_at` datetime,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+DROP TABLE IF EXISTS `schema_migrations`;
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) DEFAULT NULL,
+  UNIQUE KEY `idx_schema_migrations_version` (`version`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `person_id` int(11) NOT NULL,
+  `balance` decimal(14,2) NOT NULL DEFAULT '0.00',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `transactions`;
+CREATE TABLE `transactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `card_id` int(11) DEFAULT NULL,
+  `account_id` int(11) NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `merchant` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `type` varchar(45) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `cards`;
+CREATE TABLE `cards` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '1',
+  `number` char(15) NOT NULL,
+  `expiration` date NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `companies`;
 CREATE TABLE `companies` (
@@ -37,20 +68,7 @@ CREATE TABLE `companies` (
   `name` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `created_at` datetime,
-  `updated_at` datetime,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS `transactions`;
-CREATE TABLE `transactions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `card_id` int(11) NOT NULL,
-  `amount` decimal(10,2) NOT NULL,
-  `merchant` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `type` varchar(45) NOT NULL,
-  `created_at` datetime,
-  `updated_at` datetime,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
